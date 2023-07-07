0937be1875d88484709dfef2d375c24513eadbfa
Henry Ripple <hripple46@gmail.com>
2023-07-07 12:52:32 -0500

    added function to pull posts from a group onClick

--- src/GroupList.jsx
+++ src/GroupList.jsx
@@ -22,6 +22,7 @@ export default function GroupList({ userId, token }) {
           return response.json();
         })
         .then((data) => {
+          console.log("Groups", data);
           return data.groupNames;
         })
         .then((groupNames) => {
@@ -34,12 +35,40 @@ export default function GroupList({ userId, token }) {
       navigate("/");
     }
   };
+
+  const getPosts = (groupId) => {
+    //get posts from backend
+    try {
+      fetch("http://localhost:3000/groups/" + groupId + "/posts", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: "Bearer " + token,
+        },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log("Posts", data);
+          return data.posts;
+        });
+    } catch (err) {
+      //catch error and log it
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       <h1>Group List</h1>
       <ul>
         {groups.map((group) => {
-          return <li key={group}>{group}</li>;
+          return (
+            <li onClick={() => getPosts(group.id)} key={group.id}>
+              {group.name}
+            </li>
+          );
         })}
       </ul>
     </div>
