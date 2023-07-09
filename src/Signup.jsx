b27bff49970d841e481f53c1b39f3d80ab9dea8f
Henry Ripple <hripple46@gmail.com>
2023-07-09 16:07:06 -0500

    added signup component

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,7 +1,67 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Signup() {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const navigate = useNavigate();
+
+  const createUser = (e) => {
+    e.preventDefault();
+    //send fetch request to create user
+    fetch("http://localhost:3000/users/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((response) => {
+        //check if response is ok
+        if (!response.ok) {
+          //throw error if incorrect credentials
+          throw new Error("Try Again");
+        }
+        //if response is ok, return response
+        return response.json();
+      })
+      .then(() => {
+        navigate("/");
+      });
+  };
+
   return (
     <div>
-      <h1>Signup</h1>
+      <form className="w-1/3 justify-center flex flex-col">
+        <label className="w-full" htmlFor="username">
+          Username:
+          <input
+            className="w-full bg-gray-200 rounded-md   "
+            onChange={(e) => setUsername(e.target.value)}
+            type="text"
+            name="username"
+          />
+        </label>
+        <label className="w-full" htmlFor="password">
+          Password:
+          <input
+            className="w-full bg-gray-200 rounded-md "
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            name="password"
+          />
+        </label>
+        <input
+          className="w-full bg-green-200 rounded-md mt-4 hover:cursor-pointer"
+          onClick={createUser}
+          type="submit"
+          value="Signup"
+        />
+      </form>
     </div>
   );
 }
