0fb7903babe73b9425322752fd25d1ecef2a81b5
Henry Ripple <hripple46@gmail.com>
2023-07-14 18:43:03 -0500

    admin can click notif to see user details

--- src/AdminRequests.jsx
+++ src/AdminRequests.jsx
@@ -1,7 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import AlertIcon from "./assets/AlertIcon";
 export default function AdminRequests({ user }) {
   const [pendingRequests, setPendingRequests] = useState(null);
+  const [showPending, setShowPending] = useState(false);
+  const ref = useRef(); // Create a ref
+
   useEffect(() => {
     if (!user) {
       return;
@@ -10,11 +13,52 @@ export default function AdminRequests({ user }) {
     }
   }, [user]);
 
+  // Click outside to hide pending users
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setShowPending(false);
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+
+  //this function will show the pending users
+  const showPendingUsers = () => {
+    if (showPending) {
+      return (
+        <div
+          ref={ref}
+          className="absolute -bottom-8 bg-gray-200 p-4 rounded-md"
+        >
+          {pendingRequests.map((user) => {
+            return (
+              <div key={user}>
+                <h1>{user}</h1>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+  };
+
+  //display the number of pending requests
   const displayRequests = () => {
     if (pendingRequests.length > 0) {
       return (
-        <div className="relative bg-red-200 rounded-full p-1">
+        <div
+          onClick={() => setShowPending(true)}
+          className="relative bg-red-200 rounded-full p-1 hover:bg-gray-200"
+        >
           <AlertIcon />
+          {showPendingUsers()}
 
           <p className="absolute top-0 right-2">{pendingRequests.length}</p>
         </div>
