13340b30fe5ec06d59a598482adb7c1adf91455b
Henry Ripple <hripple46@gmail.com>
2023-07-20 15:57:24 -0500

    changed colors of posts

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -7,17 +7,17 @@ export default function Posts({ posts, currentUser }) {
       <li
         className={`p-4  rounded-lg shadow-lg mb-4 w-fit ${
           post.user && currentUser._id === post.user._id
-            ? "bg-blue-200 ml-auto"
+            ? "bg-blue-400 ml-auto"
             : post.post.recommends
-            ? "bg-green-200 mr-auto"
-            : "bg-red-200 mr-auto"
+            ? "bg-green-500 mr-auto"
+            : "bg-red-500 mr-auto"
         }`}
         key={post.id}
       >
-        <h3>
+        <h3 className="text-white">
           {post.user ? post.user.username : "Anonymous"}
           {post.post.recommends ? " recommends " : " does not recommend "}
-          {post.post.movie}
+          <h3 className="inline text-amber-200">{post.post.movie}</h3>
         </h3>
       </li>
     );
