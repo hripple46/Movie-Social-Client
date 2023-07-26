61157203e69dfa77b4fd9007f6cdcb4912157ad5
Henry Ripple <hripple46@gmail.com>
2023-07-26 15:37:54 -0500

    fixed extra prevent default

--- src/NewPassword.jsx
+++ src/NewPassword.jsx
@@ -14,8 +14,7 @@ export default function NewPassword() {
     setUserToken(tokenParam);
   }, []);
 
-  const resetUserPassword = (e) => {
-    e.preventDefault();
+  const resetUserPassword = () => {
     //fetch reset password route
     fetch(
       "https://billowing-dawn-923.fly.dev/users/reset-password/" + userToken
@@ -42,7 +41,13 @@ export default function NewPassword() {
 
   return (
     <div className="absolute flex items-center justify-center w-full h-full">
-      <form className="w-1/3 justify-center flex flex-col">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          resetUserPassword();
+        }}
+        className="w-1/3 justify-center flex flex-col"
+      >
         <label htmlFor="password">Password:</label>
         <input
           className="w-full bg-gray-200 rounded-md "
@@ -59,7 +64,6 @@ export default function NewPassword() {
         />
         <button
           className="w-full bg-green-200 rounded-md mt-4 hover:cursor-pointer"
-          onClick={resetUserPassword}
           type="submit"
         >
           Reset Password
